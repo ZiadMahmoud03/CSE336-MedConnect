f1a15cc3e7f9813a70750dc3172e8fccdca871b7
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2024-11-05 19:43:02 +0200

    Fixed merge issue

--- config/db-conn-setup.php
+++ config/db-conn-setup.php
@@ -1,40 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-    return (object) array(
-
-        // Application configuration
-        'SITENAME'     => "MedConnect",
-        'APPROOT'      => dirname(dirname(__FILE)),
-        'URL_ROOT'      => 'http://localhost/HOSPITALDONATIONS', // Adjust if hosted on a server or subfolder
-        'URL_SUBFOLDER' => '', // Leave empty if accessing directly as a folder
-
-        // Database configuration
-        'DB_HOST' => 'localhost',
-        'DB_USER' => 'root',
-        'DB_PASS' => '',
-        'DB_NAME' => 'medconnect',
-
-        // Database tables
-        'DB_ADDRESS_TABLE'=> 'Address',
-        'DB_PERSON_TABLE'=> 'Person',
-        'DB_USER_TABLE'=> 'User',
-        'DB_HOSPITALADMIN_TABLE'=> 'HospitalAdmin',
-        'DB_ITEM_TABLE'=> 'Item',
-        'DB_MEDICINE_TABLE'=> 'Medicine',
-        'DB_EQUIPMENT_TABLE'=> 'Equipment',
-        'DB_DONATION_TABLE'=> 'Donation',
-        'DB_DONATION_DETAILS_TABLE'=> 'DonationDetails',
-        'DB_EVENT_TABLE'=> 'Event',
-        'DB_EVENT_DETAILS_TABLE'=> 'EventDetails',
-        'DB_VOLUNTEER_DETAILS_TABLE'=> 'VolunteerDetails',
-
-        // Routes can be defined here if needed
-    );
-?>
-
-<?php
->>>>>>> cc488f8cd445985c0f1d853b4bd69a9c3db0b4b4
 $configs = require "config.php";
 $conn = new mysqli($configs->DB_HOST, $configs->DB_USER);
 
@@ -81,8 +45,4 @@ function run_select_query($query, $echo = false): mysqli_result|bool
     return $result;
 }
 
-<<<<<<< HEAD
 // $conn->close();
-=======
-// $conn->close();
->>>>>>> cc488f8cd445985c0f1d853b4bd69a9c3db0b4b4
