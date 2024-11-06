868187c92bacc4cb263ee624227777d348289996
Nadine <nadinehishameg@gmail.com>
2024-11-06 22:05:39 +0200

    Changed the National ID to be a text field

--- View/RegisterDonor.php
+++ View/RegisterDonor.php
@@ -7,6 +7,7 @@
     <title>Registeration Form</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 
     
 </head>
@@ -14,7 +15,7 @@
 
 <body>
     <div class="form-container">
-        <form action="index.php?action=register" method="post">
+        <form action="/register" method="post" id="registrationForm">
         <h2>REGISTER</h2>
         <div class="social-container">
                     <a href="#" class="social facebook"><i class="fab fa-facebook-f"></i></a>
@@ -25,10 +26,9 @@
             <input type="text" name="email" placeholder=" Enter your email" required>
             <input type="text" name="phone" placeholder=" Enter your phone number" required>
             <input type="text" name="adress" placeholder=" Enter your address" required>
+            <input type="text" name="nationalid" placeholder=" Enter your national ID" required>
             <input type="password" name="password" placeholder=" Enter your password" required>
             <input type="password" name="conpassword" placeholder=" Confirm your password" required>
-            <label for="national_id">Upload National ID:</label>
-            <input type="file" name="national_id" id="national_id" accept=".jpg, .jpeg, .png, .pdf" required>
             <button type="submit">Register</button>
             <p>Already have an account?<a href="Login.php"> Login</a></p>
         </form>
