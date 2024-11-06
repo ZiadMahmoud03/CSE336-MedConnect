b93e04b211e26d43a9843674dfe49c4437d3f6d7
Nadine <nadinehishameg@gmail.com>
2024-11-06 19:52:15 +0200

    Added Slider and Footer

--- View/Home.php
+++ View/Home.php
@@ -6,14 +6,12 @@
     <title>Home Page</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/193fff84d2.js" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> 
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <script src="script.js" crossorigin="anonymous"></script>
 </head>
 <body class="home">
     <div class="navbar">
         <div class="logo">
-            <img src="Logo.png" alt="Logo">
+            <img src="Images\Logo.png" alt="Logo">
         </div>
         <div class="buttons">
             <button>Donate Money</button>
@@ -28,8 +26,195 @@
             <i class="fa-solid fa-user" style="color: #000000;"></i>
         </div>
     </div>
-    <div class="slider-container">  
-    <button class="prev" onclick="changeSlide(-1)">&#10094;</button>  
+    <h2>Most Recent</h2>
+    <div class="slider-container">
+        <div class="wrapper">
+            <!-- Left button -->
+            <i class="prev-btn fas fa-angle-left" onclick="slideLeft()"></i>
+
+            <!-- Carousel container -->
+            <ul class="carousel">
+                <li class="card">
+                    <div class="img">
+                        <img src="Images\kisspng-57357-hospital-dar-al-fouad-cancer-child-5b3e67bbbbbc02.222815191530816443769.jpg" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+            </ul>
+
+            <!-- Right button -->
+            <i class="next-btn fas fa-angle-right" onclick="slideRight()"></i>
+        </div>
+    </div>
+    
+    <h2>Most Donated</h2>
+    <div class="slider-container">
+        <div class="wrapper">
+            <!-- Left button -->
+            <i class="prev-btn fas fa-angle-left" onclick="slideLeft()"></i>
+
+            <!-- Carousel container -->
+            <ul class="carousel">
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+            </ul>
+
+            <!-- Right button -->
+            <i class="next-btn fas fa-angle-right" onclick="slideRight()"></i>
+        </div>
+    </div> 
+
+    <h2>Hospitals</h2>
+    <div class="slider-container">
+        <div class="wrapper">
+            <!-- Left button -->
+            <i class="prev-btn fas fa-angle-left" onclick="slideLeft()"></i>
+
+            <!-- Carousel container -->
+            <ul class="carousel">
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+                <li class="card">
+                    <div class="img">
+                        <img src="" alt="img" draggable="false">
+                    </div>
+                    <h4>hi</h4>
+                </li>
+            </ul>
+
+            <!-- Right button -->
+            <i class="next-btn fas fa-angle-right" onclick="slideRight()"></i>
+        </div>
+    </div> 
+
+
+    <footer class="footer">
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h4>About Us</h4>
+            <p></p>
+        </div>
+
+        <div class="footer-section links">
+            <h4>Quick Links</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Donate Now</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Volunteer</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-section contact">
+            <h4>Contact Us</h4>
+            <ul>
+                <li><strong>Email:</strong></li>
+                <li><strong>Phone:</strong></li>
+                <li><strong>Address:</strong> </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="footer-bottom">
+        <p>&copy; 2024 MedConnect. All rights reserved.</p>
+    </div>
+</footer>
+
     
 
 </body>
