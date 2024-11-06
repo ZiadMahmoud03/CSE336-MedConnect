00c9415bff169ebf508f87c1a228f790d4361f9e
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2024-11-06 17:38:01 +0200

    Added methods to Person, implementation will be completed soon

--- Model/UserModels/Person.php
+++ Model/UserModels/Person.php
@@ -15,5 +15,23 @@ abstract class Person {
         $this->address = $address;
     }
 
+    public function login(string $email, string $password): bool {
+        // Check if email and password match
+        return true;
+    }
+
+    public function createAccount(string $email, string $password): bool {
+        // Create a new account
+        //funtion tepmorarily returns boolean value, will be modified later
+        return true;
+    }
+
+    public function updateProfile(string $email, string $password): bool {
+        // Update profile details
+        //funtion tepmorarily returns boolean value, will be modified later
+        return true;
+        
+    }
+
 }
 ?>
