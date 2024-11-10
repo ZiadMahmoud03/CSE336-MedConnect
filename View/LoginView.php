90c4be7d8635954aa42af6966c6c163dcf4c9a71
Nadine <nadinehishameg@gmail.com>
2024-11-10 14:21:05 +0200

    Corrected the value of email

--- View/LoginView.php
+++ View/LoginView.php
@@ -16,7 +16,7 @@
         <h2>LOGIN</h2>
             <input type="text" name="email" placeholder=" Enter your email" required>
             <input type="password" name="password" placeholder=" Enter your password" required>
-            <input type="hidden" name="login_type" valu="email">
+            <input type="hidden" name="login_type" value="email">
             <button type="submit">Login</button>
             <p>Don't have an account? <a href="SignupView.php"> Register</a></p>
         </form>
