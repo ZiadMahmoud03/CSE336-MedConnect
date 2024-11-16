f9189e7732d5e0f2611e93dc9d1915b165b379e1
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2024-11-16 13:56:42 +0200

    Added description attribute and abstract function to get description

--- Model/ItemModels/Item.php
+++ Model/ItemModels/Item.php
@@ -4,13 +4,16 @@ abstract class Item {
     protected int $itemID;
     protected string $name;
     protected int $quantityAvailable;
+    protected string $description; 
 
-    public function __construct(int $itemID, string $name, int $quantityAvailable) {
+    public function __construct(int $itemID, string $name, int $quantityAvailable, string $description) {
         $this->itemID = $itemID;
         $this->name = $name;
         $this->quantityAvailable = $quantityAvailable;
+        $this->description = $description; 
     }
 
     abstract public function checkAvailability(); 
-}
 
+    abstract public function getDescription(): string;
+}
